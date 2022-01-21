a24fc8a8d922c8beef768d0e6b311f582bab6766
Perebonilado <eradiribonnie@gmail.com>
2022-01-21 05:05:22 +0000

    added a footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -4,10 +4,10 @@ const Footer = () => {
     return ( 
     <footer className={`${FooterStyles['container']}`}>
         <div>
-            <a href='https://github.com/Perebonilado'><i className='fab fa-github'></i></a>
-            <a href='https://twitter.com/mylohebron'><i className='fab fa-twitter'></i></a>
-            <a href='https://www.linkedin.com/in/perebonilado-eradiri-25632314a'><i className='fab fa-linkedin-in'></i></a>
-            <a href='https://instagram.com/mylohebron'><i className='fab fa-instagram'></i></a>
+            <a href='https://github.com/Perebonilado' target='_blank' rel='noreferrer'><i className='fab fa-github'></i></a>
+            <a href='https://twitter.com/mylohebron' target='_blank' rel='noreferrer'><i className='fab fa-twitter'></i></a>
+            <a href='https://www.linkedin.com/in/perebonilado-eradiri-25632314a' target='_blank' rel='noreferrer'><i className='fab fa-linkedin-in'></i></a>
+            <a href='https://instagram.com/mylohebron' target='_blank' rel='noreferrer'><i className='fab fa-instagram'></i></a>
         </div>
         <p>Portfolio Built By <span>&copy; Eradiri Richard</span> using <span>Next js</span></p>
     </footer>
