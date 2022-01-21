a86eccc3e0fd4dbe52423ba8d1112b6cfb05ee3e
Perebonilado <eradiribonnie@gmail.com>
2022-01-21 22:58:55 +0000

    server error fix

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:3000' : 'https://richard-eradiri-portfolio.vercel.app'
+export const server = dev ? 'http://localhost:3000' : 'https://richard-portfolio-ei2bptmnu-perebonilado.vercel.app'
