741d233f4aadb5bbd2517b1ea2a57e2342a0ca3b
Perebonilado <eradiribonnie@gmail.com>
2022-01-21 14:52:29 +0000

    fixed error in database

--- ProjectsData/data.js
+++ ProjectsData/data.js
@@ -7,7 +7,7 @@ export const data = [
         description: `This project was a no brainer for me. I have a high penchant for music and the first thing I wanted to build was a music player.
         I really want to combine my years of experience in the music industry with tech. I believe there are many opportunities, especially with the advent of web3. A musician knows a musicians problems, as such,
         I believe, slowly but surely, I will make an impact in the music industry using tech. I built this using vanilla CSS and JavaScript, utilizing core programming concepts as well as ensuring clean code.`,
-        url: 'https://res.cloudinary.com/dt0wfaxft/image/upload/v1642515708/IMG-3077_m7ynpe.jpg',
+        url: 'https://res.cloudinary.com/dt0wfaxft/image/upload/v1642802816/3ED0DB34-0D61-4D65-B5B3-01CBD44CD590_ruhahj.jpg',
         github: 'https://github.com/Perebonilado/Spotify-Clone',
         projectLink: 'https://perebonilado.github.io/Spotify-Clone/'
     },
@@ -18,7 +18,7 @@ export const data = [
         description: `This is a self service application allowing users communicate in order to resolve escalations rasied as tickets.
         A ticket might be an issue that needs resolving, as such, with the use of the chat feature, users can communicate with each other based on a ticket raised and track the status of that ticket. The ticket ID is entered and the issue can be resolved using 
         this web application. This web app was built using React js. I utilized the context api, react hooks such as useState and useEffect to build this project. The design was implemented using vanilla CSS as it was translated from an Adobe XD file by me. I built this product for Crown Interactive LTD, a software company based in Lagos, Nigeria.`,
-        url: 'https://res.cloudinary.com/dt0wfaxft/image/upload/v1642515708/IMG-3077_m7ynpe.jpg',
+        url: 'https://res.cloudinary.com/dt0wfaxft/image/upload/v1642802815/7A11518B-1A0C-42FC-AB49-7D17A7FF35A4_m0gl5t.jpg',
         github: 'https://github.com/Perebonilado/cicod-self-service-react',
         projectLink: 'https://wfm-ticket.netlify.app'
     },
@@ -30,8 +30,8 @@ export const data = [
         description: `This is a landing page for a crypto trading platform in Africa. I enjoyed working on this project as I quite often stay in the know on the crypto markets.
         I utilized an API to fetch data on current crypto prices. I engineered this using Vanilla Js and Vanilla CSS. It is fully responsive and renders crypto data in real time. This 
         application allows users to carry out pair to pair trading of crypto currencies.`,
-        url: 'https://res.cloudinary.com/dt0wfaxft/image/upload/v1642515708/IMG-3077_m7ynpe.jpg',
-        github: 'https://github/Perebonilado/pezofoundation',
+        url: 'https://res.cloudinary.com/dt0wfaxft/image/upload/v1642802816/D03E3E09-F8E1-440E-83AF-2482A0826A00_guxfqi.jpg',
+        github: 'https://github.com/Perebonilado/pezofoundation',
         projectLink: 'https://perebonilado.github.io/pezofoundation/'
     },
 
