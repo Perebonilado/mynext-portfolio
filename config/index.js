fa9a37053b8600cff7d38dd2dfe1754ce711efec
Perebonilado <eradiribonnie@gmail.com>
2022-01-22 00:23:39 +0000

    a commit

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:3000' : 'https://mynext-portfolio.vercel.app'
+export const server = dev ? 'http://localhost:3000' : 'https://mynext-portfolio-jt54p524j-perebonilado.vercel.app'
