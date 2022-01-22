9d995538bad8654304284ecf504ec8bd17630dc9
Perebonilado <eradiribonnie@gmail.com>
2022-01-22 07:09:55 +0000

    server fixes

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:3000' : 'https://vercel.com/perebonilado/richard-portfolio'
+export const server = dev ? 'http://localhost:3000' : 'https://my-new-portfolio-henna.vercel.app'
